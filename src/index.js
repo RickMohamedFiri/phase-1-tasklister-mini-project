35ec04b903fe6a4e77dd26484865f6b93aea184c
Rick Mohamed <rickmohaa@gmail.com>
2023-06-28 12:00:24 +0300

    new commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  let form = document.getElementById('create-task-form');
+  let input = document.getElementById('new-task-description');
+  let todos = document.getElementById('tasks');
+
+  const addTodo=(e)=>{
+    e.preventDefault()
+    let todoItem = document.createElement('li')
+    todoItem.innerText=input.value;
+    todos.appendChild(todoItem)
+
+  }
+  form.addEventListener('submit', addTodo)
+
 });
